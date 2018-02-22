9a60d2a418ec5d6955a2f2d2e8d0a0a852b3424b
leenaschaumann <leena.schaumann@gmail.com>
2018-02-22 11:08:48 +0200

    database luotu

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,17 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Main {
     public static void main(String[] args) {
+        try (Connection con =
+                     (Connection) DriverManager.getConnection("jdbc:mysql://localhost:3306/tentti?useSSL=false",
+                             "root", "leenaschaumann")) {
+            System.out.println("Connection created");
 
-        System.out.println("MOi");
+            System.out.println("MOi");
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
